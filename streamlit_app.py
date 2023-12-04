a83f80b65ad692c1dd5e53a1906eafc05a0d5ca5
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2023-12-03 19:42:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,7 +119,7 @@ def run_conversation(prompt):
         model="gpt-4-1106-preview",
         messages=messages,
                 )  # get a new response from the model where it can see the function response
-        return function_response
+        return second_response
         
 st.set_page_config(page_title='Multifamily AI IRR')
 st.title('Multifamily AI IRR')
