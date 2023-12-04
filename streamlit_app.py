21c23b089eae520c1ef7ddbfd2f02f4582127cd7
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2023-12-03 19:49:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,9 +114,9 @@ def run_conversation(prompt):
                 "content": function_response,
             }
         )
-        
+        st.write(messages)
         second_response = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
+        model="gpt-4-1106-preview",
         messages=messages,
                 )  # get a new response from the model where it can see the function response
         return second_response
